f0eb385fa3872a8e7b333a9fcdc32971e8d5fc4f
Kirillov Alexander <saratovsource@gmail.com>
2012-12-19 14:13:20 +0400

    initial commit

---
+++ lib/jquery_address-rails.rb
@@ -0,0 +1,7 @@
+require "jquery_address-rails/version"
+
+module JqueryAddress
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/jquery_address-rails/version.rb
@@ -0,0 +1,5 @@
+module JqueryAddress
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
